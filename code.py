3f93773c4f76d87c073ea87bb9e2baecff97d698
CapstoneCSE137 <satyanarayana70755@gmail.com>
2025-08-12 18:35:51 +0530

    Initial upload for Phase 1

---
+++ code.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv("NFHS-5-KA-Karnataka.csv")
+
+# Filter for Bagalkot district
+district_data = df[df['District'] == 'Bagalkot']
+
+# Display first 5 rows
+print(district_data.head())
+
+# Save filtered data to new CSV
+district_data.to_csv("Bagalkot_health_data.csv", index=False)
+
+df.info()
+
+df.discribe()
